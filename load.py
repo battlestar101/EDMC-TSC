487e95273d61454cc183d81ee55cc78f78bcf60c
battlestar101 <54249133+battlestar101@users.noreply.github.com>
2023-01-21 17:37:36 -0500

    Update load.py

--- load.py
+++ load.py
@@ -10,7 +10,7 @@ def plugin_start3(plugin_dir: str) -> str:
 def dashboard_entry(cmdr: str, is_beta: bool, entry):
     destination = entry["Destination"]["Name"]
     r = requests.get("https://www.edsm.net/api-v1/system", params={'systemName': destination})
-    EDMCTSC.exists.set(f"Target system: {'Exists' if (r.text != '[]') else 'Does NOT exist 🫃'}")
+    EDMCTSC.exists.set(f"Target system: {'Exists' if (r.text != '[]') else 'Does NOT exist'}")
 
 def plugin_app(parent: tk.Frame) -> tk.Label:
     return tk.Label(parent, textvariable=EDMCTSC.exists)
