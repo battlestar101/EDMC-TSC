6acdc2cf62d2277e919a8df34d7f5074882a8406
battlestar101 <54249133+battlestar101@users.noreply.github.com>
2023-01-21 17:27:51 -0500

    Add files via upload

---
+++ load.py
@@ -0,0 +1,16 @@
+import requests
+import tkinter as tk
+
+class EDMCTSC():
+    exists = tk.StringVar()
+
+def plugin_start3(plugin_dir: str) -> str:
+    return "EDMC-TSC"
+
+def dashboard_entry(cmdr: str, is_beta: bool, entry):
+    destination = entry["Destination"]["Name"]
+    r = requests.get("https://www.edsm.net/api-v1/system", params={'systemName': destination})
+    EDMCTSC.exists.set(f"Target system: {'Exists' if (r.text != '[]') else 'Does NOT exist 🫃'}")
+
+def plugin_app(parent: tk.Frame) -> tk.Label:
+    return tk.Label(parent, textvariable=EDMCTSC.exists)
